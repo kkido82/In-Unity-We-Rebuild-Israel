a68166af66a32297c86d7350fa3e3c00bf94066c
kkido82 <idokerner@gmail.com>
2023-10-17 23:36:52 +0300

    main content added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'In-Unity-We-Rebuild-Israel';
+  institutions = [{
+    name: 'United Hazalah',
+    href: 'https://1221.org.il/',
+    imgurl: 'assets/united_hazala.webp',
+    text: 'Help Us Raise Funds For ',
+    targetText: 'Raised Already 150,000 USD'
+  },
+  {
+    name: 'eshkol',
+    href: 'https://eshkol.info/',
+    imgurl: 'assets/hevel_eshkol.jpg',
+    text: 'Help Us Raise Funds For ',
+    targetText: 'Raised Already 150,000 USD'
+  },
+  {
+    name: 'The Association for the Israel Soldiers',
+    href: 'https://www.ufis.org.il/',
+    imgurl: 'assets/swords-of-iron-war-eng.jpg',
+    text: 'Help Us Raise Funds For ',
+    targetText: 'Raised Already 150,000 USD'
+  },
+  {
+    name: 'The Sha`ar HaNegev Regional Council',
+    href: 'https://www.sng.org.il/',
+    imgurl: 'assets/shaar_hanegev.jpg',
+    text: 'Help Us Raise Funds For ',
+    targetText: 'Raised Already 150,000 USD'
+  },
+  {
+    name: 'Soldiers of Gold',
+    href: 'https://www.facebook.com/GoldSoldiers/',
+    imgurl: 'assets/soldiers_of_gold.jpg',
+    text: 'Help Us Raise Funds For ',
+    targetText: 'Raised Already 150,000 USD'
+  }];
 }
